2492c46414f4b38d24eebf7c8d651fe81d6635eb
mahabubhossain <mahabub.hossain@gmail.com>
2016-12-19 18:04:14 -0500

    First commit

---
+++ src/main/java/edu/mum/domain/Member.java
@@ -0,0 +1,138 @@
+package edu.mum.domain;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+
+@Entity 
+public class Member {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="member_id")
+	private long id;
+	
+	@Column(nullable = true)
+	private String dob;
+	
+	@Column(nullable = false)
+	private String email;
+	
+	@Column(nullable = false)
+	private Date created;
+	
+	@Column(length = 16)
+	private String firstName;
+	@Column(length = 16)
+	private String lastName;
+	
+	@Column(nullable = true)
+ 	private int age;
+	@Column(length = 32, nullable = true)
+	private String title;
+	
+	@Column(nullable = true)
+ 	private int memberNumber;
+	
+	@OneToMany(fetch=FetchType.EAGER, cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+    private Set<Item> boughtItems = new HashSet<Item>();
+
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	@OneToOne(fetch=FetchType.EAGER,  cascade = CascadeType.ALL) 
+ 	@JoinColumn(name="memberId") 
+ 	UserCredentials userCredentials;
+ 	
+ 
+     @OneToMany(mappedBy="member",fetch=FetchType.EAGER, cascade = { CascadeType.PERSIST, CascadeType.MERGE })  
+     private Set<Address> addresses = new HashSet<Address>();
+
+ 	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+ 	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getMemberNumber() {
+		return memberNumber;
+	}
+	public void setMemberNumber(int memberNumber) {
+		this.memberNumber = memberNumber;
+	}
+	
+ 	public UserCredentials getUserCredentials() {
+		return userCredentials;
+	}
+	public void setUserCredentials(UserCredentials userCredentials) {
+		this.userCredentials = userCredentials;
+	}
+	public Set<Address> getAddresses() {
+		return addresses;
+	}
+	public void setAddresses(Set<Address> addresses) {
+		this.addresses = addresses;
+	}
+ 	public void addAddress(Address address) {
+		this.addresses.add(address);
+		address.setMember(this);
+	}
+  }
---
+++ src/main/java/edu/mum/domain/Order.java
@@ -0,0 +1,109 @@
+package edu.mum.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+public class Order implements Serializable {
+	private Long id = null;
+	private int version = 0;
+
+	private String orderNumber;
+
+	private List<OrderItem> items = new ArrayList<OrderItem>();
+
+	private OrderPayment payment = new OrderPayment();
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "member_id")
+	private Member member; // / customer info
+
+	private int orderType; //store pickup or online order
+	
+	private int orderStatus;
+	
+	public Member getMember() {
+		return member;
+	}
+
+	public void setMember(Member member) {
+		this.member = member;
+	}
+
+	public int getOrderType() {
+		return orderType;
+	}
+
+	public void setOrderType(int orderType) {
+		this.orderType = orderType;
+	}
+
+	public int getOrderStatus() {
+		return orderStatus;
+	}
+
+	public void setOrderStatus(int orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+
+	public Order() {
+	}
+
+	public Order(String orderNumber, List<OrderItem> items, OrderPayment payment) {
+		this.orderNumber = orderNumber;
+		this.items = items;
+		this.payment = payment;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public String getOrderNumber() {
+		return orderNumber;
+	}
+
+	public void setOrderNumber(String orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+
+	public List<OrderItem> getItems() {
+		return items;
+	}
+
+	public void setItems(List<OrderItem> items) {
+		this.items = items;
+	}
+
+	public OrderPayment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(OrderPayment payments) {
+		this.payment = payments;
+	}
+
+	public void addOrderItem(OrderItem orderItem) {
+		this.items.add(orderItem);
+		orderItem.setOrder(this);
+	}
+
+}
